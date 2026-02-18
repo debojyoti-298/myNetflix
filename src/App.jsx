379e535295dfb56c04c7450b631bbf6336e88ea5
debojyoti-298 <debojyoti298@gmail.com>
2026-02-18 11:20:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='text-3xl font-bold text-green-800'>
+        Hello Everyone! Let's make netflix.
+    </div>
+  )
+}
+
+export default App
